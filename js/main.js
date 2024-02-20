049748ec304c5329666d48b095f9d13a8fc35e0e
Khabui <bnkha2000@gmail.com>
2024-02-20 17:00:33 +0700

    Add new project Porfolio

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-$(document).ready(function () {
-  $(".slide").addClass("owl-carousel");
-  $(".slide .owl-carousel").owlCarousel({
-    loop: true,
-    nav: false,
-    autoplay: true,
-    dots: true,
-    margin: 0,
-    navRewind: false,
-    touchDrag: true,
-    items: 1,
-  });
-});
+// $(document).ready(function () {
+//   $(".slide").addClass("owl-carousel");
+//   $(".slide .owl-carousel").owlCarousel({
+//     loop: true,
+//     nav: false,
+//     autoplay: true,
+//     dots: true,
+//     margin: 0,
+//     navRewind: false,
+//     touchDrag: true,
+//     items: 1,
+//   });
+// });
