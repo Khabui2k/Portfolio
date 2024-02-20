56ea5e07d7694480cc5fb4d2ebb5e59bdd334f9a
Khabui <bnkha2000@gmail.com>
2024-02-20 16:57:52 +0700

    Add new project Porfolio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  $(".slide").addClass("owl-carousel");
+  $(".slide .owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    autoplay: true,
+    dots: true,
+    margin: 0,
+    navRewind: false,
+    touchDrag: true,
+    items: 1,
+  });
+});
